20cc3d336ce48054aa1f405a19161dd8387136ae
Limali-22 <lithminilimali@gmail.com>
2025-11-14 22:07:28 +0530

    Create LinearSearch.java
    Phase 1: Created LinearSearch.java with array generator

---
+++ LinearSearch.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class LinearSearch {
+
+    // This creates the same random array every time
+    private static int[] generateArray(int size) {
+        Random rand = new Random(42);
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(10000);
+        }
+        return arr;
+    }
+}
