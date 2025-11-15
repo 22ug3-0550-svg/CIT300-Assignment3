d22332cce77e62cdc7305efd130e520a2cf388b6
Viraji371 <virajisudheera47@gmail.com>
2025-11-14 23:18:03 -0800

    Create BinarySearch.java
    Phase 1: BinarySearch.java with array

---
+++ BinarySearch.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    private static int[] generateArray(int size) {
+        Random rand = new Random(42);
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(10000);
+        }
+        return arr;
+    }
+}
